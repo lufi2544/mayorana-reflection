acd0bd1865d9e1d31ce60856445de7336b0fb0c1
Juanes Rayo <lufi2544@gmail.com>
2026-01-13 17:25:47 +0100

    starte the refactor for the reflection

--- source/reflection_includes.h
+++ source/reflection_includes.h
@@ -10,9 +10,56 @@
 #define MY_ENUM(params)
 #define MY_PROPERTY(params)
 
+enum type_kind
+{
+	Type_u8,
+	Type_u16,
+	Type_u32,
+	Type_u64,
+	Type_f32,
+	Type_BOOL,
+	
+	Type_Struct,
+};
+
+
+struct type_definition;
+
 struct member_definition
 {
-	u8 type;
+	type_definition *type;
 	char* name;
 	u32 offset;
+};
+
+/*
+struct vec3
+{
+    float x, y, z;
+};
+
+const member_definition VEC3_MEMBERS[] = {
+    { "x", offsetof(vec3, x), &TYPE_F32_DEF },
+    { "y", offsetof(vec3, y), &TYPE_F32_DEF },
+    { "z", offsetof(vec3, z), &TYPE_F32_DEF },
+};
+
+const type_definition VEC3_TYPE = {
+    TYPE_STRUCT,
+    sizeof(vec3),
+    .struct_type = {
+        VEC3_MEMBERS,
+        3
+    }
+};
+*/
+
+struct type_definition
+{
+	type_kind kind;
+	u32 size;
+	
+	// this is temporary for structs
+	member_definition *members;
+	u32 member_count;		
 };
