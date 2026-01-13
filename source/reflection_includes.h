268033bf3da6566f293a9030d37c85e115c2392e
Juanes Rayo <lufi2544@gmail.com>
2026-01-13 17:46:38 +0100

    Updated the documentation for the implementation

--- source/reflection_includes.h
+++ source/reflection_includes.h
@@ -27,16 +27,23 @@ struct type_definition;
 
 struct member_definition
 {
-	type_definition *type;
 	char* name;
+	type_definition *type;
 	u32 offset;
 };
 
 /*
+
+1- we first have to add the macros to the desired classes.
+
+MY_STRUCT(all)
 struct vec3
 {
     float x, y, z;
 };
+
+2 - then the Metadata_types are created
+
 
 const member_definition VEC3_MEMBERS[] = {
     { "x", offsetof(vec3, x), &TYPE_F32_DEF },
@@ -44,7 +51,7 @@ const member_definition VEC3_MEMBERS[] = {
     { "z", offsetof(vec3, z), &TYPE_F32_DEF },
 };
 
-const type_definition VEC3_TYPE = {
+const type_definition TYPE_VEC3_DEF = {
     TYPE_STRUCT,
     sizeof(vec3),
     .struct_type = {
@@ -52,6 +59,31 @@ const type_definition VEC3_TYPE = {
         3
     }
 };
+
+
+struct GameData
+{
+   f32 health;
+   vec3 location;
+};
+
+
+const member_definition GAME_DATA_MEMBERS []= 
+{
+   { "health", offsetof(GameData, health), &TYPE_F32_DEF}
+   { "location", offsetof(GameData, location), &TYPE_VEC3_DEF}
+};
+
+
+const type_definition GAME_DATA_TYPE = 
+{
+  TYPE_STRUCT,
+  sizeof(GameData),
+  GAME_DATA_MEMBERS,
+2
+}
+
+
 */
 
 struct type_definition
